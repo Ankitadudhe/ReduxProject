263df78c3c1c641b9cce3d2af492cf809354562a
Ankitadudhe <ankitadudhe95@gmail.com>
2020-03-17 10:09:22 +0530

    .

--- React-Reduxproject/redux/src/Reducer/Reducer.js
+++ React-Reduxproject/redux/src/Reducer/Reducer.js
@@ -2,6 +2,8 @@ const countReducer = (state = 0, action) => {
     switch (action.type) {
         case 'INCREASE':
             return state + action.payload;
+        case 'DECREASE':
+            return state - action.payload;    
         default:
             return state;
     }
